f3a2e9aa6de547764165134c3e41415800ba5c60
z4qx <git@z4qx.eu>
2015-01-10 00:19:03 +0100

    Update SQL DB\n Create php login/register form

--- SQL/create_db_site.sql
+++ SQL/create_db_site.sql
@@ -3,7 +3,7 @@ CREATE TABLE user (id_user INT  AUTO_INCREMENT NOT NULL,
 password VARCHAR(15),
 login VARCHAR(25),
 role INT,
-entreprise_id_entreprise INT,
+id_entreprise INT NOT NULL,
 PRIMARY KEY (id_user) ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS entreprise ;
@@ -19,7 +19,6 @@ date_immat_rcs DATETIME,
 forme_juridique VARCHAR(255),
 num_tva VARCHAR(50),
 activitee TEXT,
-user_id_user INT NOT NULL,
 id_adresse INT NOT NULL,
 PRIMARY KEY (id_entreprise) ) ENGINE=InnoDB;
 
@@ -34,7 +33,6 @@ adresse_complementaire VARCHAR(255),
 ville VARCHAR(255),
 PRIMARY KEY (id_adresse) ) ENGINE=InnoDB;
 
-ALTER TABLE user ADD CONSTRAINT FK_user_entreprise_id_entreprise FOREIGN KEY (entreprise_id_entreprise) REFERENCES entreprise (id_entreprise);
+ALTER TABLE user ADD CONSTRAINT FK_user_id_entreprise FOREIGN KEY (id_entreprise) REFERENCES entreprise (id_entreprise);
 
-ALTER TABLE entreprise ADD CONSTRAINT FK_entreprise_user_id_user FOREIGN KEY (user_id_user) REFERENCES user (id_user);
 ALTER TABLE entreprise ADD CONSTRAINT FK_entreprise_id_adresse FOREIGN KEY (id_adresse) REFERENCES adresse (id_adresse);
