cf18e39a02f27ed3000865e4b833e883af40d8de
Timothee <tim.robert@free.fr>
2015-01-20 18:12:41 +0100

    add

---
+++ pas_trop_long/templates_c/46ac34eff4d854cb71bf7018ab8eca123bf47aaa.file.index.tpl.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2015-01-20 18:08:21
+         compiled from "/Users/Timohee/Desktop/Data4All/pas_trop_long/templates/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:120193558154be8b8559a243-43423436%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '46ac34eff4d854cb71bf7018ab8eca123bf47aaa' => 
+    array (
+      0 => '/Users/Timohee/Desktop/Data4All/pas_trop_long/templates/index.tpl',
+      1 => 1421773191,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '120193558154be8b8559a243-43423436',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_54be8b855e0594_24622231',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54be8b855e0594_24622231')) {function content_54be8b855e0594_24622231($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+
+<?php }} ?>
