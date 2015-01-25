cd28487d9a46fa78f9356328f72f18712fbb2db5
Socold <socold@hotmail.fr>
2015-01-25 23:21:21 +0100

    inscrustation du caroussel
    Signed-off-by: Socold <socold@hotmail.fr>

---
+++ version_smarty/templates_c/391eeaf08d11c3e9981b788bd552685ce56fe289.file.section_titre_principal.tpl.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2015-01-25 19:31:45
+         compiled from "/opt/lampp/htdocs/Data4All/version_smarty/templates/body/section_titre_principal.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:132833292354c53691d538a1-81189132%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '391eeaf08d11c3e9981b788bd552685ce56fe289' => 
+    array (
+      0 => '/opt/lampp/htdocs/Data4All/version_smarty/templates/body/section_titre_principal.tpl',
+      1 => 1421777328,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '132833292354c53691d538a1-81189132',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_54c53691d540f1_12544547',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54c53691d540f1_12544547')) {function content_54c53691d540f1_12544547($_smarty_tpl) {?><section id="titrePrincipal">
+	<div id="titreHome">
+		Data 4 All 
+		<br>
+		<span>L'Open Data des Entreprises</span>
+	</div>
+
+	
+
+</section><?php }} ?>
