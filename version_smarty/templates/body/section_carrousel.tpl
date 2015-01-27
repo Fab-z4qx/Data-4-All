15aae4da355ce650a041f0481dfc9f687f6fb331
Socold <socold@hotmail.fr>
2015-01-27 14:02:43 +0100

    Caroussel incrustation2
    Signed-off-by: Socold <socold@hotmail.fr>

--- version_smarty/templates/body/section_carrousel.tpl
+++ version_smarty/templates/body/section_carrousel.tpl
@@ -1,10 +1,24 @@
 <section id="carrousel">
 
-	<ul class="bxslider">
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	</ul>
+<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+		<script type="text/javascript" src="js/simple.carousel.js"></script>
+	
+		<script type="text/javascript">
+			jQuery(document).ready(function() {
+	
+				$("ul.caroussel1").simplecarousel({
+					auto: 4000,
+					pagination: false
+				});
+			});
+		</script>
+	
+			<ul class="caroussel1">
+				<li><img src="images/image2.png" /></li>
+				<li><img src="images/image1.png" /></li>
+				<li><img src="images/guillaume.jpg" /></li>
+				<li><img src="images/tim.png" /></li>
+			</ul>
+	
 
 </section>
--- version_smarty/templates_c/94d9465b6e780f44b72e6453c689795e0898f8b1.file.section_carrousel.tpl.php
+++ version_smarty/templates_c/94d9465b6e780f44b72e6453c689795e0898f8b1.file.section_carrousel.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2015-01-26 23:27:17
+<?php /* Smarty version Smarty-3.1.13, created on 2015-01-27 11:54:22
          compiled from "/opt/lampp/htdocs/Data4All/version_smarty/templates/body/section_carrousel.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:176753538654c53691d56ac6-60682063%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '94d9465b6e780f44b72e6453c689795e0898f8b1' => 
     array (
       0 => '/opt/lampp/htdocs/Data4All/version_smarty/templates/body/section_carrousel.tpl',
-      1 => 1422311120,
+      1 => 1422356059,
       2 => 'file',
     ),
   ),
@@ -21,11 +21,26 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_54c53691d57409_20348691')) {function content_54c53691d57409_20348691($_smarty_tpl) {?><section id="carrousel">
 
-	<ul class="bxslider">
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	  <li><img src="/images/logoefrei.png" /></li>
-	</ul>
+<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+		<script type="text/javascript" src="js/simple.carousel.js"></script>
+	
+		<script type="text/javascript">
+			jQuery(document).ready(function() {
+	
+				$("ul.caroussel1").simplecarousel({
+					auto: 4000,
+					pagination: false
+				});
+			});
+		</script>
+	
+			<ul class="caroussel1">
+				<li><img src="images/image2.png" /></li>
+				<li><img src="images/image1.png" /></li>
+				<li><img src="images/guillaume.jpg" /></li>
+				<li><img src="images/tim.png" /></li>
+			</ul>
+	
 
-</section><?php }} ?>
+</section>
+<?php }} ?>
