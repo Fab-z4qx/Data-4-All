cdcb58a2dd571d27b97e4fa11fa8d4af00b5b279
Socold <socold@hotmail.fr>
2015-02-03 00:11:09 +0100

    intégration page accueil 2
    Signed-off-by: Socold <socold@hotmail.fr>

---
+++ version_smarty/htdocs/login.php
@@ -0,0 +1,24 @@
+<!-- Page login -->
+
+<?php
+	include("common.inc.php");
+	//Nouvelle objet smarty
+	require(ROOT_DIR.INCLUDES.'data4all.inc.php');
+	$smarty =new Smarty_datat4all();
+
+
+	require(ROOT_DIR.INCLUDES.'fonctions.php');
+	$CSS_TAB = inser_css();
+	$JS_TAB = inser_js();
+
+	$smarty->assign('js_tab', $JS_TAB);
+	$smarty->assign('css_tab', $CSS_TAB);
+
+	$smarty->assign('header', 'login');
+	$smarty->assign('footer', 'index');
+	
+	require(ROOT_DIR.INCLUDES.'lib/recaptchalib.php');
+	$smarty->registerPlugin('function', 'echo_captcha','echo_captcha');
+	
+	$smarty->display('login.tpl');
+?>
