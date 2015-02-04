3ce937d217db98b1fd8ca61fdd47f56730d82a48
Socold <socold@hotmail.fr>
2015-02-04 15:52:20 +0100

    page d'accueil
    Signed-off-by: Socold <socold@hotmail.fr>

--- VFinal_MVC_Bootstrap/index.php
+++ VFinal_MVC_Bootstrap/index.php
@@ -3,7 +3,7 @@
 session_start();
  
 //On se connecte à MySQL
-mysql_connect('localhost', 'root', 'root');
+mysql_connect();
 //mysql_select_db('tests');
  
 //On inclut le logo du site et le menu
@@ -13,11 +13,11 @@ mysql_connect('localhost', 'root', 'root');
 //On inclut le contrôleur s'il existe et s'il est spécifié
 if (!empty($_GET['page']) && is_file('controller/'.$_GET['page'].'.php'))
 {
-        include 'controller/'.$_GET['page'].'.php';
+        include 'controllers/'.$_GET['page'].'.php';
 }
 else
 {
-        include 'controller/accueil.php';
+        include 'controllers/accueil.php';
 }
  
 //On inclut le pied de page
