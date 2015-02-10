756209def0bd7755b507a8d5bdf74109b624622f
Me <alain.sp93@gmail.com>
2015-02-11 00:28:01 +0100

    Création des tables

--- VFinal_MVC_Bootstrap/app/core/database.php
+++ VFinal_MVC_Bootstrap/app/core/database.php
@@ -9,7 +9,7 @@ class Database {
          self::$_instance = new Database();
       return self::$_instance;
    }
-
+	
    protected function __construct() 
    {
 			$this->_OS = strtoupper(substr(php_uname("s"), 0, 3));
@@ -32,7 +32,8 @@ class Database {
          }
 		$this->createDB($dbname);
 		$this->_db = new PDO('mysql:host='.$host.';dbname='.$dbname.';port='.$port.'',''.$user.'', ''.$password.'', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8')); 
-
+		
+		$this->executeQueryFile(_PATH_."/app/BDDSite.sql");
       }
       catch(Exception $e)
       {   // En cas d'erreur, on affiche un message et on arrête tout
@@ -57,4 +58,18 @@ class Database {
 		$sql = "CREATE DATABASE IF NOT EXISTS ".$dbName;
 		$bdd->exec($sql);
    }
+   
+   private function executeQueryFile($filesql) {
+		$query = file_get_contents($filesql);
+		$array = explode(";\n", $query);
+		
+		$b = true;
+		for ($i=0; $i < count($array) ; $i++) {
+			$str = $array[$i];
+			if ($str != '') {
+				 $str .= ';';
+				 $this->_db->query($str);  
+			}  
+		}
+}
 }
