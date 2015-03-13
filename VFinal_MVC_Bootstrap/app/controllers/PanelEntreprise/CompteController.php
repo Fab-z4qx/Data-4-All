c34d1fd52423d9da09304634fc8078f9c05a2cab
z4qx <git@z4qx.eu>
2015-03-13 17:05:20 +0100

    ControllerCompte update for display info

--- VFinal_MVC_Bootstrap/app/controllers/PanelEntreprise/CompteController.php
+++ VFinal_MVC_Bootstrap/app/controllers/PanelEntreprise/CompteController.php
@@ -6,8 +6,6 @@ class CompteController extends Controller
 {
    public function display() 
    {
-   	 //$pdo = Database::getInstance();
-   	
    	 $entreprise = new Entreprise();
    	 $this->smarty->assign('nom_entreprise', $_SESSION['info']['nom_entreprise']);
    	 $this->smarty->assign('email_entreprise', $_SESSION['info']['email_entreprise']);
@@ -16,12 +14,12 @@ class CompteController extends Controller
    	 $this->smarty->assign('fax_entreprise', $_SESSION['info']['fax_entreprise']);
    	 $this->smarty->assign('forme_juridique_entreprise', $_SESSION['info']['forme_juridique_entreprise']);
    	 $this->smarty->assign('type_offre', $entreprise->getTypeOffreByName($_SESSION['info']['type_offre']));
-   	 //$this->smarty->assign('adresse',$entreprise->getAdresse($_SESSION['info']['id_adresse']));
-   	 
-       echo '<pre>';
-   	 	print_r($entreprise->getAdresse($_SESSION['info']['id_adresse']));
-   	 echo '</pre>';
-   	 
+       $adresse = $entreprise->getAdresse($_SESSION['info']['id_adresse']);
+   	 $this->smarty->assign('adresse',$adresse['adresse']);
+       $this->smarty->assign('adresse_complementaire',$adresse['adresse_complementaire']);
+       $this->smarty->assign('code_postal',$adresse['code_postal']);
+       $this->smarty->assign('ville',$adresse['ville']);
+       $this->smarty->assign('pays',$adresse['pays']);
    	 $this->smarty->display(_TPL_ENT_.'compte.tpl');
    }
 }
