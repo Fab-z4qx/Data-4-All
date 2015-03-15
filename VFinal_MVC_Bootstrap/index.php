e44f998c89ed0eb8a43d3862be82fc06f7adfb35
m4gyars <contact.f37@gmail.com>
2015-03-15 16:24:57 +0100

    Fixing problem with old code in index controller

--- VFinal_MVC_Bootstrap/index.php
+++ VFinal_MVC_Bootstrap/index.php
@@ -4,7 +4,6 @@ include('app/config/config_init.php');
  
 //On inclut le contrôleur s'il existe et s'il est spécifié
 function startController()
-
 {
 	include (_CTRL_PUBLIC_.ucfirst($_GET['page']).'Controller.php'); //ucfirst() met la 1er lettre en majuscule pour respecter la convention objet 
     $className = ucfirst($_GET['page']).'Controller';
@@ -12,7 +11,8 @@ function startController()
     if(isset($_GET['action']))
     	$actionName = $_GET['action'];
     
-    if(empty($_GET['action'])){
+    if(empty($_GET['action']))
+    {
     	$controleur->display();
     }
     else
@@ -20,8 +20,10 @@ function startController()
     	$controleur->$actionName();
     }
 }
+
 /* LIST OF CONTROLLER WITHOUT INCLUDE TPL */
-$controler_with_no_include  = array('connect', 'formulaire');	
+//$controler_with_no_include  = array('connect', 'formulaire');	
+/*
 if (!empty($_GET['page']) && is_file(_CTRL_PUBLIC_.ucfirst($_GET['page']).'Controller.php'))
 {
 	include (_CTRL_PUBLIC_.ucfirst($_GET['page']).'Controller.php'); //ucfirst() met la 1er lettre en majuscule pour respecter la convention objet 
@@ -31,21 +33,22 @@ if (!empty($_GET['page']) && is_file(_CTRL_PUBLIC_.ucfirst($_GET['page']).'Contr
     if(isset($_GET['action']))
     	$actionName = $_GET['action'];
     
-    if(empty($_GET['action'])){
+    if(empty($_GET['action']))
+    {
     	$controleur->display();
     }
     else
     {
     	$controleur->$actionName();
     }
-}
+}*/
 
 $controler_with_no_include  = array('connect', 'formulaire');	
 if (!empty($_GET['page']) && is_file(_CTRL_PUBLIC_.ucfirst($_GET['page']).'Controller.php'))
 {
-	if( in_array($_GET['page'], $controler_with_no_include) )  //si dans le tableau in include pas les TPL
+	if( in_array($_GET['page'], $controler_with_no_include) )  //si dans le tableau on include pas les TPL
 	{
-		startController();
+		startController(); // On lance le controller
 	}
 	else 
 	{
@@ -54,7 +57,7 @@ if (!empty($_GET['page']) && is_file(_CTRL_PUBLIC_.ucfirst($_GET['page']).'Contr
 		include _TPL_COMMON_.'footer.tpl';
 	}  
 }
-else
+else // Si pas de controleur specifique on affiche l'index
 {
 	include _TPL_COMMON_.'head.tpl';
     include _CTRL_PUBLIC_.'IndexController.php';
